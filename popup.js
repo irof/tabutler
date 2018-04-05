5690308e4227ca04aa911b3e5ab0e4ed4a8d0bde
irof <irof@hogedriven.net>
2018-04-05 14:42:38 +0900

    eslintとフォーマット

--- popup.js
+++ popup.js
@@ -1,27 +1,27 @@
 let sweepButton = document.getElementById("sweep");
-sweepButton.onclick = function(element) {
-  chrome.tabs.query({}, function(tabs) {
-    let remove_urls = ["chrome://newtab/"];
+sweepButton.onclick = function () {
+    chrome.tabs.query({}, function (tabs) {
+        let remove_urls = ["chrome://newtab/"];
 
-    for (let tab of tabs) {
-      if (remove_urls.includes(tab.url)) {
-        chrome.tabs.remove(tab.id);
-      }
-      remove_urls.push(tab.url);
-    }
-  });
-}
+        for (let tab of tabs) {
+            if (remove_urls.includes(tab.url)) {
+                chrome.tabs.remove(tab.id);
+            }
+            remove_urls.push(tab.url);
+        }
+    });
+};
 
 let orderlyButton = document.getElementById("orderly");
-orderlyButton.onclick = function() {
-  chrome.tabs.query({}, function(tabs) {
-    tabs.sort(function(a, b) {
-      return a.url >= b.url ? 1 : -1;
+orderlyButton.onclick = function () {
+    chrome.tabs.query({}, function (tabs) {
+        tabs.sort(function (a, b) {
+            return a.url >= b.url ? 1 : -1;
+        });
+        for (let i in tabs) {
+            chrome.tabs.move(tabs[i].id, {index: parseInt(i)});
+        }
     });
-    for (let i in tabs) {
-      chrome.tabs.move(tabs[i].id, {index: parseInt(i)});
-    }
-  });
-}
+};
 
 
